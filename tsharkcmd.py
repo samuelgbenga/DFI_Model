12e1bc5e5f91d0e9f1f0a538996d38f77fa6c4f5
samuelgbenga <samuelgbenga972@gmail.com>
2022-12-01 14:48:35 +0100

    another one

--- tsharkcmd.py
+++ tsharkcmd.py
@@ -9,10 +9,10 @@ import os
 
 def Capture_DFI():
     
-    os.system("""tshark -i any -c 25 -f "tcp dst port 443 or 8443" -w logfile.pcap -T fields -E header=y -E separator=, -E occurrence=f -e ip.proto -e ip.flags -e ip.frag_offset -e ip.flags.mf -e ip.flags.df -e ip.flags.rb -e ip.checksum -e ip.dsfield -e ip.dsfield.dscp -e ip.dsfield.ecn  -e frame.len -e ip.len > logfile.csv""")
+    os.system("""tshark -i 1 -c 25 -f "tcp dst port 443 or 8443" -w logfile.pcap -T fields -E header=y -E separator=, -E occurrence=f -e ip.proto -e ip.flags -e ip.frag_offset -e ip.flags.mf -e ip.flags.df -e ip.flags.rb -e ip.checksum -e ip.dsfield -e ip.dsfield.dscp -e ip.dsfield.ecn  -e frame.len -e ip.len > logfile.csv""")
    
 
 def Capture_DPI():
-    os.system("""tshark -i any -c 10 -f "tcp dst port 80 or 8080" -w dpi_packet.pcap""")
+    os.system("""tshark -i 1 -c 10 -f "tcp dst port 80 or 8080" -w dpi_packet.pcap""")
 
     
